559634aa9c1c039a2431e238f56853e5412b33e7
Bogdan <bogdan10050@gmail.com>
2023-06-16 15:19:34 +0300

    add Odessa photos

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const initialCards = [
   {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    name: "Молдаванка",
+    link: "https://cdn.fishki.net/upload/post/2019/01/30/2856758/1f35052999b3b30e3ac95f02d5d8f621.jpg",
   },
   {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    name: "Аркадия",
+    link: "https://ic.pics.livejournal.com/alise_alice/17440242/17819/17819_900.jpg",
   },
   {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    name: "Посёлок Котовского",
+    link: "https://sun6-23.userapi.com/s/v1/if1/Qm6dKAYZaFi5nu3fNRqdo5lx_dp-8Yba3YpXK984rIzHWQSHdyj6qMKk6twjsGP8jMI5zg.jpg?size=1280x1280&quality=96&crop=258,0,1280,1280&ava=1",
   },
   {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    name: "Ривьера",
+    link: "https://www.menslife.com/upload/iblock/94d/5.jpg",
   },
   {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    name: "Дерибасовская",
+    link: "https://megotel.ru/images/hotels/93447/2.jpg",
   },
   {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-  {
-    name: "Мост",
-    link: "https://img.freepik.com/free-photo/palace-bridge-morning_1398-1428.jpg",
+    name: "Оперный Театр",
+    link: "https://img.freepik.com/free-photo/night-view-opera-house-odessa_1153-3600.jpg",
   },
 ];
 
